7b93aca4eb8b92b03c973a1325614a8b17962002
Ethan Ho <eho@tacc.utexas.edu>
2021-08-19 15:56:36 -0500

    Interface tweaks

--- alphafold/data/pipeline_mod.py
+++ alphafold/data/pipeline_mod.py
@@ -7,7 +7,7 @@ from absl import logging
 from alphafold.data import parsers
 from alphafold.data import templates
 from alphafold.data.tools.cli import *
-from alphafold.data.pipeline import make_sequence_features, make_msa_features
+from alphafold.data.pipeline import make_sequence_features
 import numpy as np
 
 # Internal import (7716).
@@ -42,64 +42,27 @@ class ModularDataPipeline:
   uniclust30_database_path: str = None
   small_bfd_database_path: str = None
 
-  def init_runners(self,
-               jackhmmer_binary_path: str,
-               hhblits_binary_path: str,
-               hhsearch_binary_path: str,
-               uniref90_database_path: str,
-               mgnify_database_path: str,
-               bfd_database_path: Optional[str],
-               uniclust30_database_path: Optional[str],
-               small_bfd_database_path: Optional[str],
-               pdb70_database_path: str,
-               template_featurizer: templates.TemplateHitFeaturizer,
-               use_small_bfd: bool,
-               mgnify_max_hits: int = 501,
-               uniref_max_hits: int = 10000):
-    """Deprecated"""
-    pass
-  
-  def out_path(self, fname):
-      return os.path.join(self.msa_output_dir, fname)
-    
-  def write(self, data, fname) -> str:
-    fp = self.out_path(fname)
-    with open(fp, 'w') as f:
-      f.write(data)
-    return fp
-
-  def load_input_fasta(self, input_fasta_path):
-    with open(input_fasta_path) as f:
-      input_fasta_str = f.read()
-    input_seqs, input_descs = parsers.parse_fasta(input_fasta_str)
-    if len(input_seqs) != 1:
-      raise ValueError(
-          f'More than one input sequence found in {input_fasta_path}.')
-    input_sequence = input_seqs[0]
-    input_description = input_descs[0]
-    return (input_sequence, input_description)
-
   def jackhmmer_uniref90(self, input_fasta_path: str):
-    return jackhmmer_uniref90(
-      input_fasta_path=input_fasta_path, 
-      jackhmmer_binary_path=self.jackhmmer_binary_path,
-      uniref90_database_path=self.uniref90_database_path,
-      output_dir=self.msa_output_dir
-    )
-    return self.write(result, 'uniref90_hits.sto')
-
-  def jackhmmer_mgnify(self, input_fasta_path: str):
-    return jackhmmer_mgnify(
+    return jackhmmer(
       input_fasta_path=input_fasta_path, 
       jackhmmer_binary_path=self.jackhmmer_binary_path, 
-      mgnify_database_path=self.mgnify_database_path, 
-      mgnify_max_hits=self.mgnify_max_hits,
+      database_path=self.uniref90_database_path, 
+      fname='uniref90_hits.sto',
+      output_dir=self.msa_output_dir
+    )
+
+  def jackhmmer_mgnify(self, input_fasta_path: str):
+    return jackhmmer(
+      input_fasta_path=input_fasta_path, 
+      jackhmmer_binary_path=self.jackhmmer_binary_path, 
+      database_path=self.mgnify_database_path, 
+      fname='mgnify.sto',
       output_dir=self.msa_output_dir
     )
   
-  def hhsearch_pdb70(self, jackhmmer_uniref90_result_path):
+  def hhsearch_pdb70(self, jackhmmer_uniref90_hits_path):
     return hhsearch_pdb70(
-      jackhmmer_uniref90_result_path=jackhmmer_uniref90_result_path, 
+      jackhmmer_uniref90_hits_path=jackhmmer_uniref90_hits_path, 
       hhsearch_binary_path=self.hhsearch_binary_path,
       pdb70_database_path=self.pdb70_database_path, 
       uniref_max_hits=self.uniref_max_hits,
@@ -107,10 +70,11 @@ class ModularDataPipeline:
     )
   
   def jackhmmer_small_bfd(self, input_fasta_path):
-    return jackhmmer_small_bfd(
+    return jackhmmer(
       input_fasta_path=input_fasta_path, 
       jackhmmer_binary_path=self.jackhmmer_binary_path, 
-      small_bfd_database_path=self.small_bfd_database_path,
+      database_path=self.small_bfd_database_path, 
+      fname='small_bfd_hits.sto',
       output_dir=self.msa_output_dir
     )
 
@@ -135,47 +99,36 @@ class ModularDataPipeline:
       obsolete_pdbs_path=self.obsolete_pdbs_path,
       strict_error_check=self.strict_error_check
     )
+  
+  def make_msa_features(self, jackhmmer_uniref90_hits_path, jackhmmer_mgnify_hits_path,
+                        bfd_hits_path):
+    return make_msa_features(jackhmmer_uniref90_hits_path, jackhmmer_mgnify_hits_path,
+                             bfd_hits_path,
+                             mgnify_max_hits=self.mgnify_max_hits,
+                             use_small_bfd=self.use_small_bfd)
+  
+  def make_sequence_features(self, input_fasta_path):
+    input_sequence, input_description, num_res = parse_fasta_path(input_fasta_path)
+    return make_sequence_features(sequence=input_sequence, 
+                                  description=input_description, 
+                                  num_res=num_res)
 
   def process(self, input_fasta_path: str, msa_output_dir: str) -> FeatureDict:
     """Runs alignment tools on the input sequence and creates features."""
     self.msa_output_dir = msa_output_dir
-    input_sequence, input_description = self.load_input_fasta(input_fasta_path)
-    sequence_features = make_sequence_features(
-        sequence=input_sequence,
-        description=input_description,
-        num_res=len(input_sequence))
 
-    jackhmmer_uniref90_result_path = self.jackhmmer_uniref90(input_fasta_path)
-    mgnify_msa, mgnify_deletion_matrix = self.jackhmmer_mgnify(input_fasta_path)
-    hhsearch_hits_path = self.hhsearch_pdb70(jackhmmer_uniref90_result_path)
-
-    with open(jackhmmer_uniref90_result_path, 'r') as f:
-      uniref90_msa, uniref90_deletion_matrix, _ = parsers.parse_stockholm(f.read())
+    jackhmmer_uniref90_hits_path = self.jackhmmer_uniref90(input_fasta_path)
+    hhsearch_hits_path = self.hhsearch_pdb70(jackhmmer_uniref90_hits_path)
+    template_features = self.template_featurize(input_fasta_path, hhsearch_hits_path)
 
     if self.use_small_bfd:
-      jackhmmer_small_bfd_result = self.jackhmmer_small_bfd(input_fasta_path)
-      bfd_msa, bfd_deletion_matrix, _ = parsers.parse_stockholm(
-          jackhmmer_small_bfd_result)
+      bfd_hits_path = self.jackhmmer_small_bfd(input_fasta_path)
     else:
-      hhblits_bfd_uniclust_result = self.hhblits(input_fasta_path)
-      bfd_msa, bfd_deletion_matrix = parsers.parse_a3m(
-          hhblits_bfd_uniclust_result)
+      bfd_hits_path = self.hhblits(input_fasta_path)
 
-    templates_result = self.template_featurize(input_sequence, hhsearch_hits_path)
-
-    msa_features = make_msa_features(
-        msas=(uniref90_msa, bfd_msa, mgnify_msa),
-        deletion_matrices=(uniref90_deletion_matrix,
-                           bfd_deletion_matrix,
-                           mgnify_deletion_matrix))
-
-    logging.info('Uniref90 MSA size: %d sequences.', len(uniref90_msa))
-    logging.info('BFD MSA size: %d sequences.', len(bfd_msa))
-    logging.info('MGnify MSA size: %d sequences.', len(mgnify_msa))
-    logging.info('Final (deduplicated) MSA size: %d sequences.',
-                 msa_features['num_alignments'][0])
-    logging.info('Total number of templates (NB: this can include bad '
-                 'templates and is later filtered to top 4): %d.',
-                 templates_result.features['template_domain_names'].shape[0])
-
-    return {**sequence_features, **msa_features, **templates_result.features}
+    jackhmmer_mgnify_hits_path = self.jackhmmer_mgnify(input_fasta_path)
+    sequence_features = self.make_sequence_features(input_fasta_path)
+    msa_features = self.make_msa_features(jackhmmer_uniref90_hits_path,
+                                          jackhmmer_mgnify_hits_path,
+                                          bfd_hits_path)
+    return {**sequence_features, **msa_features, **template_features}
--- alphafold/data/tools/cli.py
+++ alphafold/data/tools/cli.py
@@ -9,9 +9,8 @@ import hashlib
 from typing import Mapping, Optional, Sequence
 from alphafold.data.tools import hhblits
 from alphafold.data.tools import hhsearch
-from alphafold.data.tools import jackhmmer
-from alphafold.data import parsers
-from alphafold.data import templates
+from alphafold.data.tools import jackhmmer as jackhmmer_wrapper
+from alphafold.data import parsers, templates, pipeline
 import logging
 
 DEFAULT_CACHE_DIR = '.cache'
@@ -22,14 +21,17 @@ def cli():
     pass
 
 
-def parse_fasta(input_fasta_path):
+def parse_fasta_path(input_fasta_path):
   """Given fasta file at `input_fasta_path`, calls `parsers.parse_fasta`
   returning tuple of input sequence, input description, and sequence length.
   """
   with open(input_fasta_path) as f:
       input_fasta_str = f.read()
   input_seqs, input_descs = parsers.parse_fasta(input_fasta_str)
-  return (input_seqs[0], input_descs[0], len(input_sequence))
+  if len(input_seqs) != 1:
+    raise ValueError(
+        f'More than one input sequence found in {input_fasta_path}.')
+  return (input_seqs[0], input_descs[0], len(input_seqs[0]))
 
 
 def write_output(data, fname, output_dir) -> str:
@@ -69,7 +71,7 @@ def cache_key(args, kwargs):
     return md5_hash(serialized)
     
 
-def cache_to_pckl(cache_dir=DEFAULT_CACHE_DIR, exclude_kw=None, use_pckl=True):
+def cache_to_pckl(cache_dir=None, exclude_kw=None, use_pckl=True):
     """Caches function results to pickle file. Returns a decorator factory.
     Pickled function results are cached to a path `cache_dir/func.__name__/hash`
     where hash is hashed args and kwargs (except kwargs listed in `exclude_kw`).
@@ -82,6 +84,9 @@ def cache_to_pckl(cache_dir=DEFAULT_CACHE_DIR, exclude_kw=None, use_pckl=True):
     elif isinstance(exclude_kw, str):
         exclude_kw = [exclude_kw]
     
+    if cache_dir is None:
+        cache_dir = DEFAULT_CACHE_DIR
+    
     # whether to use pickle or plain text cache
     cache_ext = 'pckl' if use_pckl is True else 'out'
     SKIP_PCKL_CACHE = os.environ.get('SKIP_PCKL_CACHE', 0)
@@ -92,8 +97,7 @@ def cache_to_pckl(cache_dir=DEFAULT_CACHE_DIR, exclude_kw=None, use_pckl=True):
             key = cache_key(args, kw)
             cache_fp = os.path.join(cache_dir, fn.__name__, f"{key}.{cache_ext}")
 
-            logging.info(f"using cache_fp={cache_fp}")
-            logging.info(f"SKIP_PCKL_CACHE={SKIP_PCKL_CACHE}")
+            logging.debug(f"using cache_fp={cache_fp} (SKIP_PCKL_CACHE={SKIP_PCKL_CACHE})")
 
             if os.path.exists(cache_fp) and not SKIP_PCKL_CACHE:
                 logging.info(f"using cache at {cache_fp} instead of running {fn.__name__}")
@@ -111,7 +115,7 @@ def cache_to_pckl(cache_dir=DEFAULT_CACHE_DIR, exclude_kw=None, use_pckl=True):
             logging.info(f"saving results to {cache_fp}")
             if use_pckl:
                 with open(cache_fp, 'wb') as f:
-                    pickle.dump(result, f)
+                    pickle.dump(result, f, protocol=4)
             else:
                 with open(cache_fp, 'w') as f:
                     f.write(result)
@@ -124,76 +128,38 @@ def cache_to_pckl(cache_dir=DEFAULT_CACHE_DIR, exclude_kw=None, use_pckl=True):
 
 # ------------------------------------------------------------------------------
 
-@cli.command(name='jackhmmer_uniref90')
+@cli.command(name='jackhmmer')
 @click.option('--input-fasta-path', required=True, type=click.Path())
 @click.option('--jackhmmer-binary-path', required=True, type=click.Path())
-@click.option('--uniref90-database-path', required=True, type=click.Path())
+@click.option('--database-path', required=True, type=click.Path())
 @click.option('--output-dir', required=True, type=click.Path(file_okay=False))
-def jackhmmer_uniref90_cli(*args, **kwargs):
-    return jackhmmer_uniref90(*args, **kwargs)
+def jackhmmer_cli(*args, **kwargs):
+    return jackhmmer(*args, **kwargs)
 
 
-@cache_to_pckl(exclude_kw='output_dir')
-def jackhmmer_uniref90(input_fasta_path: str, jackhmmer_binary_path: str,
-                       uniref90_database_path: str, output_dir: str):
-  jackhmmer_uniref90_runner = jackhmmer.Jackhmmer(
+@cache_to_pckl(exclude_kw=['output_dir'])
+def jackhmmer(input_fasta_path: str, jackhmmer_binary_path: str,
+              database_path: str, fname: str, output_dir: str):
+  jackhmmer_uniref90_runner = jackhmmer_wrapper.Jackhmmer(
       binary_path=jackhmmer_binary_path,
-      database_path=uniref90_database_path)
+      database_path=database_path)
   result = jackhmmer_uniref90_runner.query(input_fasta_path)[0]['sto']
-  return write_output(result, 'uniref90_hits.sto', output_dir=output_dir)
-
-
-@cli.command(name='jackhmmer_mgnify')
-@click.option('--input-fasta-path', required=True, type=click.Path())
-@click.option('--jackhmmer-binary-path', required=True, type=click.Path())
-@click.option('--mgnify-database-path', required=True, type=click.Path())
-@click.option('--mgnify-max-hits', required=True, type=click.INT)
-@click.option('--output-dir', required=True, type=click.Path(file_okay=False))
-def jackhmmer_mgnify_cli(*args, **kwargs):
-    return jackhmmer_mgnify(*args, **kwargs)
+  return write_output(result, fname, output_dir=output_dir)
 
 
 @cache_to_pckl(exclude_kw='output_dir')
-def jackhmmer_mgnify(input_fasta_path: str, jackhmmer_binary_path: str,
-                     mgnify_database_path: str, mgnify_max_hits, 
-                     output_dir: str):
-  jackhmmer_mgnify_runner = jackhmmer.Jackhmmer(
-      binary_path=jackhmmer_binary_path,
-      database_path=mgnify_database_path)
-  result = jackhmmer_mgnify_runner.query(input_fasta_path)[0]['sto']
-  write_output(result, 'mgnify_hits.sto', output_dir=output_dir)
-
-  mgnify_msa, mgnify_deletion_matrix, _ = parsers.parse_stockholm(result)
-  mgnify_msa = mgnify_msa[:mgnify_max_hits]
-  mgnify_deletion_matrix = mgnify_deletion_matrix[:mgnify_max_hits]
-
-  return (mgnify_msa, mgnify_deletion_matrix)
-
-
-@cache_to_pckl(exclude_kw='output_dir')
-def hhsearch_pdb70(jackhmmer_uniref90_result_path, hhsearch_binary_path: str,
+def hhsearch_pdb70(jackhmmer_uniref90_hits_path, hhsearch_binary_path: str,
                    pdb70_database_path: str, output_dir: str,
                    uniref_max_hits):
-  with open(jackhmmer_uniref90_result_path, 'r') as f:
-    jackhmmer_uniref90_result = f.read()
+  with open(jackhmmer_uniref90_hits_path, 'r') as f:
+    jackhmmer_uniref90_hits = f.read()
   hhsearch_pdb70_runner = hhsearch.HHSearch(
       binary_path=hhsearch_binary_path,
       databases=[pdb70_database_path])
   uniref90_msa_as_a3m = parsers.convert_stockholm_to_a3m(
-      jackhmmer_uniref90_result, max_sequences=uniref_max_hits)
+      jackhmmer_uniref90_hits, max_sequences=uniref_max_hits)
   result = hhsearch_pdb70_runner.query(uniref90_msa_as_a3m)
   return write_output(result, 'pdb70_hits.hhr', output_dir=output_dir)
-
-
-@cache_to_pckl(exclude_kw='output_dir')
-def jackhmmer_small_bfd(input_fasta_path: str, jackhmmer_binary_path: str,
-                        small_bfd_database_path: str, output_dir: str):
-  jackhmmer_small_bfd_runner = jackhmmer.Jackhmmer(
-      binary_path=jackhmmer_binary_path,
-      database_path=small_bfd_database_path)
-  result = jackhmmer_small_bfd_runner.query(input_fasta_path)[0]['sto']
-  write_output(result, 'small_bfd_hits.a3m', output_dir=output_dir)
-  return result
 
 
 @cache_to_pckl(exclude_kw='output_dir')
@@ -204,8 +170,7 @@ def hhblits(input_fasta_path: str, hhblits_binary_path: str,
       binary_path=hhblits_binary_path,
       databases=[bfd_database_path, uniclust30_database_path])
   result = hhblits_bfd_uniclust_runner.query(input_fasta_path)['a3m']
-  write_output(result, 'bfd_uniclust_hits.a3m', output_dir=output_dir)
-  return result
+  return write_output(result, 'bfd_uniclust_hits.a3m', output_dir=output_dir)
 
 
 @cache_to_pckl()
@@ -224,11 +189,50 @@ def template_featurize(input_fasta_path, hhsearch_hits_path, mmcif_dir: str,
     obsolete_pdbs_path=obsolete_pdbs_path,
     strict_error_check=strict_error_check,
   )
-  return template_featurizer.get_templates(
+  input_sequence, _, _ = parse_fasta_path(input_fasta_path)
+  features = template_featurizer.get_templates(
       query_sequence=input_sequence,
       query_pdb_code=None,
       query_release_date=None,
-      hits=hhsearch_hits)
+      hits=hhsearch_hits).features
+  logging.info('Total number of templates (NB: this can include bad '
+                'templates and is later filtered to top 4): %d.',
+                features['template_domain_names'].shape[0])
+  return features
+
+
+@cache_to_pckl()
+def make_msa_features(jackhmmer_uniref90_hits_path, jackhmmer_mgnify_hits_path, 
+                      bfd_hits_path, mgnify_max_hits: int, use_small_bfd: bool):
+    with open(jackhmmer_uniref90_hits_path, 'r') as f:
+      uniref90_msa, uniref90_deletion_matrix, _ = parsers.parse_stockholm(f.read())
+
+    with open(jackhmmer_mgnify_hits_path, 'r') as f:
+      mgnify_msa, mgnify_deletion_matrix, _  = parsers.parse_stockholm(f.read())
+
+    with open(bfd_hits_path, 'r') as f:
+      bfd_hits = f.read()
+
+    mgnify_msa = mgnify_msa[:mgnify_max_hits]
+    mgnify_deletion_matrix = mgnify_deletion_matrix[:mgnify_max_hits]
+
+    if use_small_bfd:
+      bfd_msa, bfd_deletion_matrix, _ = parsers.parse_stockholm(bfd_hits)
+    else:
+      bfd_msa, bfd_deletion_matrix = parsers.parse_a3m(bfd_hits)
+
+    msa_features = pipeline.make_msa_features(
+        msas=(uniref90_msa, bfd_msa, mgnify_msa),
+        deletion_matrices=(uniref90_deletion_matrix,
+                           bfd_deletion_matrix,
+                           mgnify_deletion_matrix))
+
+    logging.info('Uniref90 MSA size: %d sequences.', len(uniref90_msa))
+    logging.info('BFD MSA size: %d sequences.', len(bfd_msa))
+    logging.info('MGnify MSA size: %d sequences.', len(mgnify_msa))
+    logging.info('Final (deduplicated) MSA size: %d sequences.',
+                 msa_features['num_alignments'][0])
+    return msa_features
 
 
 if __name__ == '__main__':
